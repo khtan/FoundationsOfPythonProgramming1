39143247e3d24bb466fbc3e3e35e6179c1373212
Kwee Tan <tan.k.h@juno.com>
2020-06-13 14:25:49 -0700

    Add test_template and test_ch17_jsonpath.java

---
+++ java/src/test/java/kwee/learn/test_template.java
@@ -0,0 +1,37 @@
+package kwee.learn;
+
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+/*
+import java.util.List;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.path.json.JsonPath;
+
+// static imports
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+*/
+
+/* 1. Notes
+*/
+public class test_template {
+    private Logger logger;
+
+    @BeforeEach
+    public void setUp(){
+       logger = Logger.getLogger(test_template.class.getName());
+    }
+// #region xxx
+// #endregion xxx
+// #region tests
+    @Test
+    public void test_1111_XXX() {
+        logger.info("hello");
+    }
+// #endregion tests
+}
