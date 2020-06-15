a706203c2b7bcae9899dbe52624be19aecab5dc2
Kwee Tan <tan.k.h@juno.com>
2020-06-14 18:02:39 -0700

    mvn does not pick up files with test_ch names. Change to TestCh and misc fixes

--- java/src/test/java/kwee/learn/TestCh08.java
+++ java/src/test/java/kwee/learn/TestCh08.java
@@ -1,9 +1,16 @@
 package kwee.learn;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-public class test_ch08 {
+public class TestCh08
+ {
+    private Logger logger;
+    @BeforeEach
+    public void setUp(){
+        logger = Logger.getLogger(TestCh08.class.getName());
+    }
     @Test
     public void test_1(){
         String rainfall_mi = "1.65, 1.46, 2.05, 3.03, 3.35, 3.46, 2.83, 3.23, 3.5, 2.52, 2.8, 1.85";
@@ -13,8 +20,7 @@ public class test_ch08 {
             var rainfall = Float.parseFloat(rain_string);
             if (rainfall > 3.0) num_rainy_months++;
         }
-        System.out.println(String.format("num_rainy_months = %d", num_rainy_months));
+        logger.info(String.format("num_rainy_months = %d", num_rainy_months));
         assertEquals(5, num_rainy_months);
     }
-
 }
--- java/src/test/java/kwee/learn/TestCh10.java
+++ java/src/test/java/kwee/learn/TestCh10.java
@@ -22,16 +22,16 @@ Deitel 15.7.2 InputStreamReader, StringReader, LineNumberReader
 
 */
 
-public class test_ch10 {
+public class TestCh10 {
     private Logger logger;
 
     @BeforeEach
     public void setUp(){
-        logger = Logger.getLogger(test_ch10.class.getName());
+        logger = Logger.getLogger(TestCh10.class.getName());
     }
     @Test
     public void test_1031_num_char_read_scanner(){
-        try(Scanner input = new Scanner(Paths.get("../../../data/school_prompt2.txt"))){
+        try(Scanner input = new Scanner(Paths.get("../data/school_prompt2.txt"))){
             var num_char = 0;
             // logger.info(String.format("delimiter: >%s<", input.delimiter()));
             while(input.hasNext()){
@@ -51,7 +51,7 @@ public class test_ch10 {
     @Test
     public void test_1031_num_char_read_bufferedreader(){
         // try(BufferedReader input = new BufferedReader(new FileReader("../../../data/school_prompt2.txt"))){ // old style
-        try(BufferedReader input = Files.newBufferedReader(Paths.get("../../../data/school_prompt2.txt"))){            
+        try(BufferedReader input = Files.newBufferedReader(Paths.get("../data/school_prompt2.txt"))){            
             var num_char = 0;
             String line;
             while((line = input.readLine()) != null){
@@ -69,7 +69,7 @@ public class test_ch10 {
     @Test
     public void test_1031_num_char_read_filesreadalllines(){
         try{
-            List<String> lines = Files.readAllLines(Paths.get("../../../data/school_prompt2.txt"));
+            List<String> lines = Files.readAllLines(Paths.get("../data/school_prompt2.txt"));
             var num_char = 0;        
             for(String line : lines){
                 num_char += line.length();
@@ -85,7 +85,7 @@ public class test_ch10 {
     @Test
     public void test_1031_num_char_read_stream_of_string(){
         // functional in steps
-        try(Stream<String> streamString = Files.lines(Paths.get("../../../data/school_prompt2.txt"))){
+        try(Stream<String> streamString = Files.lines(Paths.get("../data/school_prompt2.txt"))){
             Stream<Integer> streamInt = streamString.map( line -> line.length());
             Optional<Integer> num_char = streamInt.reduce((x,y) -> x + y);
             // logger.info(String.valueOf(num_char));
@@ -103,7 +103,7 @@ public class test_ch10 {
     @Test
     public void test_1031_num_char_read_stream_of_string2(){
         // more canonically functional
-        try(Stream<String> streamString = Files.lines(Paths.get("../../../data/school_prompt2.txt"))){
+        try(Stream<String> streamString = Files.lines(Paths.get("../data/school_prompt2.txt"))){
             int num_char = streamString
                 .map( line -> line.length())
                 .reduce((x,y) -> x + y)
