a575b8f6ef89d5d5d18a0f856747cb9907da6d2a
Kwee H Tan <tan.k.h@juno.com>
2020-09-03 22:08:29 -0700

    test_column_sheet_annotatec_brightness

--- python/tests/test_project_assgn1.py
+++ python/tests/test_project_assgn1.py
@@ -7,7 +7,10 @@ import io
 from PIL import Image
 from PIL import ImageFilter
 from PIL import ImageEnhance
-pytest.skip("skipping project_assgn1 tests, currently will show images and wait", allow_module_level=True) # currently using show to display pictures
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# pytest.skip("skipping project_assgn1 tests, currently will show images and wait", allow_module_level=True) # currently using show to display pictures
 
 # from IPython.display import display
 ''' Notes
@@ -27,13 +30,44 @@ def test_simple_show():
     image=Image.open(file)
     image.show() # this works and holds the execution until display is closed
     # display(image) # this does not bring up any display
-
 def test_blur():
     file="..\data\msi_recruitment.gif"
     image=Image.open(file)
     image = image.convert('RGB')
     blurred_image = image.filter(ImageFilter.BLUR)
     blurred_image.show()
+
+def test_crop():
+    file="..\data\msi_recruitment.gif"
+    gif_image=Image.open(file)
+    rgb_image=gif_image.convert('RGB')
+    cropped = rgb_image.crop((50,0,190,150))
+    cropped.show()
+
+def test_draw_crop_box():
+    file="..\data\msi_recruitment.gif"
+    gif_image=Image.open(file)  # drawing on gif have poor rendering
+    rgb_image=gif_image.convert('RGB')
+    drawing_object = ImageDraw.Draw(rgb_image)
+    drawing_object.rectangle((50,0,190,150), fill = None, outline = 'red')
+    rgb_image.show()
+
+def test_draw_text():
+    file="..\data\msi_recruitment.gif"
+    gif_image=Image.open(file)  # drawing on gif have poor rendering
+    rgb_image=gif_image.convert('RGB')
+    drawing_object = ImageDraw.Draw(rgb_image)
+    drawing_object.text((50,0), "hello", fill = 'red', align = 'left')
+    rgb_image.show()
+
+def test_draw_text_with_font():
+    file="..\data\msi_recruitment.gif"
+    gif_image=Image.open(file)  # drawing on gif have poor rendering
+    rgb_image=gif_image.convert('RGB')
+    drawing_object = ImageDraw.Draw(rgb_image)
+    font_object = ImageFont.truetype('../data/fanwood-webfont.ttf', 20)
+    drawing_object.text((50,0), "hello world", fill = 'yellow', align = 'left', font = font_object)
+    rgb_image.show()
 
 def test_column_sheet_brightness():
     file="..\data\msi_recruitment.gif"
@@ -43,6 +77,28 @@ def test_column_sheet_brightness():
     images = []
     for i in range(0,10):
         images.append(enhancer.enhance(i/10))
+    first_image = images[0]
+    contact_sheet = Image.new(first_image.mode, (first_image.width, 10 * first_image.height))
+    current_location = 0
+    for img in images:
+        contact_sheet.paste(img, (0, current_location))
+        current_location = current_location + 450
+    contact_sheet = contact_sheet.resize((160,900))
+    contact_sheet.show()
+
+def test_column_sheet_annotated_brightness():
+    file="..\data\msi_recruitment.gif"
+    gif_image=Image.open(file)
+    rgb_image=gif_image.convert('RGB')
+    enhancer = ImageEnhance.Brightness(rgb_image)
+    images = []
+    font_object = ImageFont.truetype('../data/fanwood-webfont.ttf', 40)
+    for i in range(0,10):
+        brightness = i/10;
+        e_image = enhancer.enhance(brightness)
+        drawing = ImageDraw.Draw(e_image)
+        drawing.text((200,10), "brightness={}".format(brightness), fill = 'black', aligh = 'left', font = font_object)
+        images.append(e_image)
     first_image = images[0]
     contact_sheet = Image.new(first_image.mode, (first_image.width, 10 * first_image.height))
     current_location = 0
