3c050c78649ac00c1ebb6e458db24498c8cee789
Kwee Tan <kwee@eaglecapsoftware.com>
2023-08-29 14:27:16 -0700

    XUnit-LanguageExt to specifically test language-ext without adding the library elsewhere

---
+++ csharp/XUnit-LanguageExt/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnit_LanguageExt
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ csharp/XUnit-LanguageExt/test_langext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+// using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using LanguageExt.Common;
+// using Newtonsoft.Json;
+// using FluentAssertions;
+
+namespace XUnit_LanguageExt
+{
+    public class Test_langext
+    {
+        private readonly ITestOutputHelper output; // helper provides interface for WriteLine only, so not logging per se
+        public Test_langext(ITestOutputHelper output) { this.output = output; }
+        static Result<int> Divide(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                return new Result<int>(new Exception("Division by zeron"));
+            }
+            return new Result<int>(numerator / denominator);
+        }
+        static void ExerciseDivide(ITestOutputHelper output, int numerator, int denominator)
+        {
+            Result<int> res = Divide(numerator, denominator);
+            var m = res.Match<int>(
+                Succ: (value) => {
+                    output.WriteLine($"Result {value}");
+                    return value;
+                },
+                Fail: (ex) => {
+                    output.WriteLine($"Error: {ex.Message}");
+                    return 0;
+                }
+            );
+            output.WriteLine($"m: {m}");
+        }
+        [Fact]
+        public void Test_0001()
+        {
+            ExerciseDivide(this.output, 10, 2);
+        }
+        [Fact]
+        public void Test_0002()
+        {
+            ExerciseDivide(this.output, 10, 0);
+        }
+    }// class
+}// namespace
+
