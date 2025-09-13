ee9121f3062d3a107f2a069e1799541d936cc655
Kwee Tan <kwee@eaglecapsoftware.com>
2025-09-12 17:37:24 -0700

    pylint and pylance messages

--- python/tests/RunestoneAcademy/test_ch17_nested.py
+++ python/tests/RunestoneAcademy/test_ch17_nested.py
@@ -1,12 +1,10 @@
+""" Tests for chapter 17. Nested Containers """
 # region imports
-import os
-import sys
 import logging
-import pytest
-import io
 import json
 import copy
 
+# pylint: disable=W0105
 ''' Notes
 1.
 2.
@@ -20,10 +18,11 @@ logger = logging.getLogger(__name__)
 # endregion helpers
 # region tests for xx.x
 def test_172_nested_dictionaries():
+    """ 17.2 Nested dictionaries """
     # A dictionary can have both integer and string keys
-    dict = {'key1': {'a':5, 'c': 90, 5:50, '5':55}, 'key2':{'b':3, 'c':"yes"}}
-    assert 55 == dict['key1']['5']
-    assert 50 == dict['key1'][5]
+    dict_a = {'key1': {'a':5, 'c': 90, 5:50, '5':55}, 'key2':{'b':3, 'c':"yes"}}
+    assert 55 == dict_a['key1']['5']
+    assert 50 == dict_a['key1'][5]
 
 def test_1731_json_loads():
     ''' 17.3 json module : json.loads and json.dumps
@@ -32,26 +31,28 @@ def test_1731_json_loads():
         Due to logger differences, have to manually view the similarities
     '''
     # 1. Start with a json encoded string, including \n
-    a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}'
-    logger.info("1 a_string:\n{}\n:a_string".format(a_string))
+    a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}' # pylint: disable=C0301
+    logger.info("1 a_string:\n%s\n:a_string", a_string)
     # 2. Use json.loads
     d = json.loads(a_string)
     typed = str(type(d))
-    logger.info("2 type: {}".format(typed))
+    # logger.info("2 type: {}".format(typed))
+    logger.info("2 type: %s", typed)
     assert "<class 'dict'>" == typed
     # 3. access dictionary keys
-    logger.info("3 d: {}".format(d.keys()))
+    logger.info("3 d: %s", d.keys())
     # 4. each line in dictionary
     for k in d:
-      logger.info("4 {}".format(k))
+        logger.info("4 %s", k)
     # 5. access inner results dictionry
     for i in range(len(d['results'])):
-      logger.info("5 {} -> {}".format(i, d['results'][i]))
+        logger.info("5 %d -> %s", i, d['results'][i])
     # 6. access dictionary element
-    logger.info("6 resultCount = {}".format(d['resultCount']))
+    logger.info("6 resultCount = %d", d['resultCount'])
     assert 25 == d['resultCount']
 
 def pretty(obj):
+    """ Pretty print a dictionary using json.dumps """
     return json.dumps(obj, sort_keys=True, indent=2)
 def test_1732_json_dumps():
     ''' This is an example of using json as a utility
@@ -75,15 +76,17 @@ def test_1732_json_dumps():
 }'''
     assert expected == pretty(d)
 def test_176_deepcopy():
+  # pylint: disable=C0301
     ''' This test is used to show that Python has a deepcopy utility to deal with nested containers.
         This strategy is different in C# bec C# requires each class to handle its own copy semantics.
         For example, ArrayList has a clone while Dictionary<T> has a copy constructor
     '''
     eoriginal = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]] # expected, unmodified
-    original = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]    
+    original  = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]
+    # original: List[Union[List[Union[str, List[str]]], str]] = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]
     shallow_copy_version = original[:]
     deeply_copied_version = copy.deepcopy(original)
-    original.append("Hi there")
+    original.append("Hi there") # type: ignore
     original[0].append(["marsupials"])
     eoriginal2 = [['canines', ['dogs', 'puppies'], ['marsupials']], ['felines', ['cats', 'kittens']], 'Hi there']
     assert eoriginal2 == original
